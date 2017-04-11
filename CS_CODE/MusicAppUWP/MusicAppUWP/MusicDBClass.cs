b1788b25772556dd19dd535f9ac5eff44036d4bf
KEUM HONG LIM <kiwizen.code@hotmail.com>
2017-04-12 10:57:42 +1200

    update a sampe project

---
+++ CS_CODE/MusicAppUWP/MusicAppUWP/MusicDBClass.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MusicAppUWP
+{
+    class MusicDBSQLService
+    {
+        private static string ConnectionString = 
+            @"Server=tcp:kiwizen-dsed.database.windows.net,1433;" + 
+             "Initial Catalog=DSED;Persist Security Info=False;User ID=kiwizen-dsed;Password=Monday99;"+
+             "MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;";
+    }
+
+
+
+
+    class MusicOwnerTable
+    {
+        static string PrimaryKey { get; } = "OwnerID";
+        static string FirstName { get; } = "FirstName";
+        static string LastName { get; } = "LastName";
+    }
+
+
+    class MusicCDClass
+    {
+        static string PrimaryKey { get; } = "CDID";
+        static string OwnerID { get; } = "OwnerIDFK";
+        static string Name { get; } = "Name";
+        static string Artist { get; } = "Artist";
+        static string Genre { get; } = "Genre";
+    }
+}
