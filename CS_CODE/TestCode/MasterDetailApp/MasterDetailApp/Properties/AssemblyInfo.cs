14849bef58ba8932771d84f451d673cf7280b954
KEUM HONG LIM <kiwizen.code@hotmail.com>
2017-04-13 09:49:27 +1200

    delete the folder SampleCode create a folder call TestCode create a new project MasterDetailApp

--- CS_CODE/TestCode/MasterDetailApp/MasterDetailApp/Properties/AssemblyInfo.cs
+++ CS_CODE/TestCode/MasterDetailApp/MasterDetailApp/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("NotesApp")]
+[assembly: AssemblyTitle("MasterDetailApp")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("NotesApp")]
-[assembly: AssemblyCopyright("Copyright ©  2015")]
+[assembly: AssemblyCompany("HP Inc.")]
+[assembly: AssemblyProduct("MasterDetailApp")]
+[assembly: AssemblyCopyright("Copyright © HP Inc. 2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
